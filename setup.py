9c88fd22bf74e4dde09cd90e33a2fc2d658b6eb0
abhi.b <abhi.b@ibm.com>
2021-10-20 13:43:32 -0400

    cleaning up redundant packages

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='tdw_transport_challenge',
       version='0.0.1',
-      install_requires=['gym', 'zmq', 'pymongo', 'Pillow', 'pandas', 'boto3', 'requests', 'numpy', 'tdw', 'magnebot==1.1.2', 'transport_challenge']
+      install_requires=['gym', 'pymongo', 'pandas']
 )
